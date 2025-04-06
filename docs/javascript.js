16ee89cb25041c39df22c0664a11639313d314b3
Cosmo Grant <cosmo.grant@postcodelottery.co.uk>
2025-04-06 23:02:13 +0100

    flip toggle symbol on each click

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,7 +1,18 @@
+const HIDE_OUTPUT_SYMBOL = "∧";
+const SHOW_OUTPUT_SYMBOL = "∨";
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggles = [...document.getElementsByClassName("toggle-output")];
   const outputs = [...document.getElementsByClassName("output")];
   toggles.forEach(toggle => toggle.addEventListener("click", function () {
     outputs.forEach(output => output.hidden = !output.hidden);
+    toggles.forEach(
+      toggle => toggle.innerHTML = (toggle.innerHTML != SHOW_OUTPUT_SYMBOL ? SHOW_OUTPUT_SYMBOL : HIDE_OUTPUT_SYMBOL)
+    );
   }))
+  const clickEvent = new Event("click");
+  toggles[0].dispatchEvent(clickEvent);
 });
+
+
